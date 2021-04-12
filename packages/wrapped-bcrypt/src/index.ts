55ea9ab0e9bf3c26faf40ff3b6f88990061f9061
sqrtthree <imsqrtthree@gmail.com>
2021-04-12 18:08:08 +0800

    refactor(wrapped-bcrypt): Refactor with promise api

--- packages/wrapped-bcrypt/src/index.ts
+++ packages/wrapped-bcrypt/src/index.ts
@@ -4,36 +4,14 @@ export function hash(
   clearText: string,
   salt?: number | string
 ): Promise<string> {
-  return new Promise((resolve, reject) => {
-    bcrypt.hash(clearText, salt || 10, (err: Error | null, result: string) => {
-      if (err) {
-        reject(err)
-        return
-      }
-
-      resolve(result)
-    })
-  })
+  return bcrypt.hash(clearText, salt || 10)
 }
 
 export function compare(
   clearText: string,
   hashString: string
 ): Promise<boolean> {
-  return new Promise((resolve, reject) => {
-    bcrypt.compare(
-      clearText,
-      hashString,
-      (err: Error | null, result: boolean) => {
-        if (err) {
-          reject(err)
-          return
-        }
-
-        resolve(result)
-      }
-    )
-  })
+  return bcrypt.compare(clearText, hashString)
 }
 
 export default {
